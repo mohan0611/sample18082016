6ee29aa5fcd14311e174c8f431a05f410a3555e6
mohan0611 <mohan0611@gmail.com>
2016-09-03 18:59:43 +0530

    The form post with List data  updated in the Timesheet controller

--- sample18082016/Controllers/TimesheetsController.cs
+++ sample18082016/Controllers/TimesheetsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace sample18082016.Controllers
 {
@@ -13,8 +15,8 @@ namespace sample18082016.Controllers
         {
             List<TimesheetEntry> obj = new List<TimesheetEntry>()
                         {
-                            new TimesheetEntry() {ProjectName ="Project 1", ActivityName = "Coding", day1 = -1, day2 = -1 },
-                            new TimesheetEntry() {ProjectName ="Project 2", ActivityName = "Coding", day1 = -1, day2 = -1 }
+                            new TimesheetEntry() {TimesheetId = 1, ProjectName ="Project 1", ActivityName = "Coding", day1 = 0, day2 = 0 },
+                            new TimesheetEntry() {TimesheetId = 24, ProjectName ="Project 2", ActivityName = "Coding"}
                         };
 
 
@@ -33,7 +35,7 @@ namespace sample18082016.Controllers
         {
             //List<TimesheetEntry> obj = new List<TimesheetEntry>()
             //            {
-            //                new TimesheetEntry() {ProjectName ="Project 1", ActivityName = "Coding", day1 = -1, day2 = -1 }
+            //                new TimesheetEntry() {P5rojectName ="Project 1", ActivityName = "Coding", day1 = -1, day2 = -1 }
             //            };
 
 
@@ -57,7 +59,7 @@ namespace sample18082016.Controllers
             //}
 
 
-
+            ViewBag.ErrorMessage = "Please fill the Project Name and Activity in Row 2";
 
             return View(obj);
         }
@@ -66,10 +68,22 @@ namespace sample18082016.Controllers
 
     public class TimesheetEntry
     {
+
+        public int TimesheetId { get; set; }
+
+        [StringLength(20)]
+        [Required]
         public string ProjectName { get; set; }
+
+        [StringLength(10, MinimumLength = 3)]
+        [Required]
         public string ActivityName { get; set; }
+
         //public string StartDate { get; set; }
+        [Range(0, 24)]
         public Decimal day1 { get; set; }
+
+        [Range(0, 24)]
         public Decimal day2 { get; set; }
 
     }
